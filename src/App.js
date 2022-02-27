d2d55139b5b03f156571ea876212a9aa3605b1c9
innu <innukhatri15@gmail.com>
2022-02-28 01:39:26 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+class App extends Component {
+  state = {
+    name: "",
+    department: "",
+    rating: "",
+    employees: [],
+    show: false
+  }
+
+  OnSubmit(event) {
+    event.preventDefault();
+    const object = {
+      name: this.state.name,
+      department: this.state.department,
+      rating: this.state.rating
+    }
+
+    this.state.employees.push(object)
+    this.setState({
+      name: "",
+      department: "",
+      rating: "",
+      show: true
+
+    })
+    console.log(this.state.employees)
+  }
+whenclick = (event) => {
+    console.log(event.target);
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  render() {
+    return (
+      <>
+        <div className='container'>
+          <h1>EMPLOYEE FEEDBACK FORM</h1>
+          <form action="">
+            <label for="name">Name : </label>
+            <input type="text" id="name" name="name" value={this.state.name}  onChange={this.whenclick} /><br></br>
+            <label for="department">Department : </label>
+            <input type="text" id="department" name="department" value={this.state.department} onChange={this.whenclick} /><br></br>
+            <label for="rating">Rating : </label>
+            <input type="number" id="rating" name="rating" value={this.state.rating} onChange={this.whenclick} /><br></br>
+
+          </form>
+          <button type="submit" onClick={this.OnSubmit.bind(this)}>Submit</button>
+        </div>
+        {this.state.show && <div className='history'>
+          {this.state.employees.length > 0 && this.state.employees.map((event) => {
+            return (<div className='employeedata'>
+              <h1>"Name"={event.name}  "Department"={event.department}  "Rating"={event.rating}</h1>
+            </div>)
+          })}
+        </div>}
+      </>
+    )
+  }
 }
+
 
 export default App;
